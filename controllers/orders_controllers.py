33daae6440c04b79865c27e2cb8677283acc82c6
kcampbell3190 <kcampbell3190@gmail.com>
2022-11-10 10:37:39 +1100

    added orders model/controllers

---
+++ controllers/orders_controllers.py
@@ -0,0 +1,75 @@
+from flask import Blueprint, request
+from init import db
+from datetime import date
+from models.orders import Order, OrderSchema
+
+
+orders_bp = Blueprint('orders', __name__, url_prefix='/orders')
+
+@orders_bp.route('/')
+
+def all_orders():
+  
+     stmt = db.select(Order)
+     orders = db.session.scalars(stmt).all()
+     return ProductSchema(many=True).dump(orders)
+
+@orders_bp.route('/<int:id>/')
+def one_order(id):
+    stmt = db.select(Order).filter_by(id=id)
+    orders = db.session.scalar(stmt)
+    if order:
+        return ProductSchema(many=True).dump(order)
+    else: 
+        return {'error': f'Order not found with id {id}'}, 404
+
+
+@orders_bp('/', methods=['POST'])
+@jwt_required()
+def create_product():
+  
+    data = OrderSchema().load(request.json)
+
+    order = Order(
+        user_id = request.json['user_id'],
+        product_id= request.json['product_id'],
+        amount = request.json['amout'],
+        status = request.json['status'],
+        quantity = request.json['quantity'],
+    )
+    # Add and commit the user to the database
+    db.session.add(order)
+    db.session.commit()
+    return OrderSchema().dump(order), 201
+    
+
+@orders_bp.route('/<int:id>/', methods=['DELETE'])
+@jwt_required()
+def delete_one_product(id):
+    authorize()
+
+    stmt = db.select(Order).filter_by(id=id)
+    order= db.session.scalar(stmt)
+    if order:
+        db.session.delete(order)
+        db.session.commit()
+        return {'message': f"Order '{order.title}' deleted successfully"}
+    else:
+        return {'error': f'Order not found with id {id}'}, 404
+
+
+@orders_bp.route('/<int:id>/', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_one_order(id):
+    stmt = db.select(Order).filter_by(id=id)
+    order = db.session.scalar(stmt)
+    if order:
+        order.user_id = request.json.get('user_id') or order.user_id
+        order.product_id = request.json.get('product_id') or order.product_id
+        order.status = request.json.get('status') or order.status
+        order.amount = request.json.get('amount') or order.amount
+        order.quantity = request.json.get('quantity') or order.quantity
+        db.session.commit()      
+        return OrderSchema().dump(orders)
+    else:
+        return {'error': f'Card not found with id {id}'}, 404
--- models/order.py
+++ models/order.py
@@ -5,4 +5,12 @@ class Order(db.Model):
     """"orders"""
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
-    
+    user_id = db.Column()
+    product_id = db.Column()
+    amount = db.Column(db.Integer, not null)
+    quantity = db.Column(db.Integer, not null)
+    status = db.Column(db.Boolean, default=False)
+
+class OrderSchema(ma.Schema):
+    class Meta: 
+        fields = ('id', 'user_id', 'product_id', 'description', 'status', 'amount', 'quantity',)
